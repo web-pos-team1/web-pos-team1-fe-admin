283265765cce6cd1d3de5c5bcf2e4a08126b20d1
mysterymrlee <100182498+mysterymrlee@users.noreply.github.com>
2023-06-12 15:52:51 +0900

    feat:piechart

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 import style from './index.module.css';
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import { mapToBE } from '@/globalFunction/mapToBE';
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const router = useRouter();
